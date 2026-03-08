94ee29ef7904b875c48d0f9cd99531b82c0d9498
benson-dylan <dylan222444@gmail.com>
2026-03-08 12:42:44 -0400

    Added pages to scores and implemented main iframe for videos

--- js/index.js
+++ js/index.js
@@ -10,3 +10,35 @@ buttons.forEach(btn => {
         document.getElementById(btn.dataset.tab).removeAttribute("hidden");
     });
 });
+
+let currentTab = 0;
+const scoreTabs = document.querySelectorAll(".score-page");
+const dots = document.querySelectorAll(".page-dot");
+
+console.log(scoreTabs.length + " " + dots.length);
+
+function showTab(index) {
+    scoreTabs[currentTab].setAttribute("hidden", "");
+    dots[currentTab].classList.remove("current-page");
+    currentTab = index;
+    scoreTabs[currentTab].removeAttribute("hidden");
+    dots[currentTab].classList.add("current-page");
+}
+
+function changeTab(direction)
+{
+    console.log("DIRECTION " + direction);
+    const next = (currentTab + direction + scoreTabs.length) % scoreTabs.length; 
+    console.log("GOING TO TAB " + next);
+    showTab(next);
+}
+
+function scrollScoreLeft()
+{
+    changeTab(-1);
+}
+
+function scrollScoreRight()
+{
+    changeTab(1);
+}
